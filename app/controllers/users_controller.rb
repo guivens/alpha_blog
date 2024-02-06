bf1a36ce1958680e85082b245cec98f158f6a5c9
Guivens <pierrecharlesguivens@gmail.com>
2024-02-06 00:40:52 -0500

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
@@ -12,6 +16,21 @@ class UsersController < ApplicationController
         else
             render :new
         end
+    end
+
+    def edit
+        @user = User.find(params[:id]) 
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your account was updated successfully"
+            redirect_to articles_path
+        else
+            render :edit
+        end
+        
     end
 
     private 
