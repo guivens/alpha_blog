3a71955beba0c3d002fd071dcaaa948329bc2fc1
Guivens <pierrecharlesguivens@gmail.com>
2024-02-08 00:33:49 -0500

    index ,show and edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
 
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+
     def index
         @users = User.all
     end
