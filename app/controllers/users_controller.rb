ae5345143bbc28efaa80a4394c950978fa88f2b4
Guivens <pierrecharlesguivens@gmail.com>
2024-02-09 02:05:39 -0500

    little changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             flash[:success] = "Welcome to the alpha blog #{@user.username} "
-            redirect_to users_path
+            redirect_to @user
         else
             render :new
         end
