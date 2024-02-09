42ad756915076e84103231f05b64e6868c781a86
Guivens <pierrecharlesguivens@gmail.com>
2024-02-09 02:26:32 -0500

    login work perfectly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        flash[:notice] = "Logged in successfuly"
+        redirect_to user
+      else
+        flash[:alert] = "something wrong"
+        redirect_to root_path
+      end
     end
 
     def destroy 
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  
 end
